e6b0efdd38e2f94101f2318711f4f266108a7a79
cprudhomme <cyril.prudhomme.ext@cibtp.fr>
2023-12-14 16:15:17 +0100

    init

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require('express')
 const path = require("path");
 const app = express()
 const port = 3000
+const {MongoClient} = require('mongodb');
+// const mongoClient = new MongoClient("mongodb://root:example@localhost?retryWrites=true&w=majority");
+const mongoClient = new MongoClient("mongodb://root:example@localhost:27017?retryWrites=true&w=majority");
 
 app.use(express.static('www'));
-app.get('/hw', function (req, res) {
-  res.send('Hello World!');
+app.get('/hw', async function (req, res) {
+  try {
+    await mongoClient.connect();
+    const results = await mongoClient.db("app").collection("mammals").find({}).toArray()
+    res.json(results)
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({message: `${e}`})
+  } finally {
+    await mongoClient.close();
+  }
+
+
 });
 
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'setup',
+    loadChildren: () => import('./setup/setup.module').then( m => m.SetupPageModule)
   }
 ];
 @NgModule({
